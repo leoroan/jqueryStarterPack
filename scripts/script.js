04235f36436fbd2695b148987a966e708138bc1e
Leandro <leoroan@gmail.com>
2023-06-28 21:17:28 -0300

    some changes to make this thing work!

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,34 @@
 console.log("hello tester!");
 
-$('.class1').text('lean');
+const url ="" ;
 
-// asi agrega una clase de css creada
-$('.class1').addClass('prueba');
+// form_listener
+
+$('#your-form').on('click', '#submitButton1', function (e) {
+  e.preventDefault();
+  
+  //alternative: instead of 'formSubmit.json' you can grab the target from the form tag: $(this).attr('action')
+  $.ajax( {
+    url: url,
+    type: 'POST',
+    data: $(this).serialize()
+  })
+    .done(function (response) {
+      // manejar la respuesta correcta a mi manera
+      $('#result').text(response.message);
+    })
+    .fail(function (request, errorType, errorMessage) {
+      //timeout, error, abort, parseerror
+      // alert(errorMessage);
+      console.log(errorType);
+      $('#result').text(errorType +" "+ errorMessage);
+    })
+    .always(function () {
+      $('#resultAlways').text('POST ejecutado');
+    });
+});
+
+
+
+
+
